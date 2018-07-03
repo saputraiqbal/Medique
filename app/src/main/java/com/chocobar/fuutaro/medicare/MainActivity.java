8b7bfae88035f6add2095f953ceb00df6942661a
Iqbal S Putra <iqbal.sp26@gmail.com>
2018-07-03 19:38:38 +0700

    Edited codes
    Refining codes phase 1

--- app/src/main/java/com/chocobar/fuutaro/medicare/MainActivity.java
+++ app/src/main/java/com/chocobar/fuutaro/medicare/MainActivity.java
@@ -1,13 +1,31 @@
 package com.chocobar.fuutaro.medicare;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.app.ProgressDialog;
+import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String SOAP_ACTION = "http://tempuri.org/CallSpExcecution";
+    private static final String NAMESPACE = "CallSpExcecution";
+    private static final String METHOD_NAME = "http://tempuri.org/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+}
+
+class myAsyncTask extends AsyncTask<Void, Void, Void>{
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
     }
 }
