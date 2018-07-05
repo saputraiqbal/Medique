db6f990430de373b65ccfc022903fd5a20a1a0cb
Iqbal S Putra <iqbal.sp26@gmail.com>
2018-07-05 08:05:28 +0700

    Minor update
    [Minor update]
    - Add documentations
    - Remove unused code lines

--- app/src/main/java/com/chocobar/fuutaro/medicare/MainActivity.java
+++ app/src/main/java/com/chocobar/fuutaro/medicare/MainActivity.java
@@ -9,41 +9,43 @@ import android.view.View;
 import android.widget.Toast;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
 import org.ksoap2.SoapEnvelope;
 import org.ksoap2.serialization.PropertyInfo;
-import org.ksoap2.serialization.SoapPrimitive;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.transport.HttpTransportSE;
 
-import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    //initialize widget objects app
     EditText inputUsername, inputPassword;
     Button btnLogin;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initialize object widgets to UI widget elements
         inputUsername = findViewById(R.id.loginUsername);
         inputPassword = findViewById(R.id.loginPassword);
         btnLogin = findViewById(R.id.btnLogin);
 
+        //initialize login button action
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //execute webservice using AsyncTask
                 new WebServiceLogin().execute(inputUsername.getText().toString(), inputPassword.getText().toString());
             }
         });
     }
 
+    //initialize AsyncTask class
     class WebServiceLogin extends AsyncTask<String, Void, Integer> {
+
+        //initialize method when AsyncTask has been executed
         @Override
         protected void onPostExecute(Integer integer) {
             if(integer == 1)
@@ -54,12 +56,15 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Maaf, ada kesalahan. Silakan laporkan pada pihak berwenang...", Toast.LENGTH_LONG).show();
         }
 
+        //initialize AsyncTask method
         @Override
         protected Integer doInBackground(String... strings) {
             Integer bitSuccessConnect = -1;
 
+            //initializing the process of request first
             SoapObject reqsWebSvc = new SoapObject(STATIC_VALUES.NAMESPACE, "CallSpExcecution");
 
+            //configuring the envelope before creating it
             PropertyInfo SPNameInfo = new PropertyInfo();
             SPNameInfo.setNamespace(STATIC_VALUES.NAMESPACE);
             SPNameInfo.setType(PropertyInfo.STRING_CLASS);
@@ -74,26 +79,35 @@ public class MainActivity extends AppCompatActivity {
             ParamValueInfo.setValue("txtUsername#"+strings[0]+"~txtPassword#"+strings[1]);
             reqsWebSvc.addProperty(ParamValueInfo);
 
+            //creating an envelope
             SoapSerializationEnvelope env = new SoapSerializationEnvelope(SoapEnvelope.VER11);
             env.setOutputSoapObject(reqsWebSvc);
             env.dotNet = true;
+
+            //creating the request
             HttpTransportSE httpTrans = new HttpTransportSE(STATIC_VALUES.URL);
 
+            //transaction for sending the request
             try {
                 httpTrans.call(STATIC_VALUES.SOAP_ACTION, env);
+                //processing the request
                 SoapObject soapResponse = (SoapObject) env.bodyIn;
 
+                //selection either SoapObject soapResponse retrieve the request or not
                 if(soapResponse.toString().equals("CallSpExcecutionResponse{}") || soapResponse == null)
                     return bitSuccessConnect;
+                //if request has been retrieved by SoapObject soapResponse
                 else{
                     String callSpExcecutionResult = soapResponse.getPropertyAsString("CallSpExcecutionResult");
                     JSONArray jArray = new JSONArray(callSpExcecutionResult);
                     String bitExeResult = jArray.getJSONObject(0).getString("bitSuccess");
                     bitSuccessConnect = Integer.parseInt(bitExeResult);
                 }
+            //if transaction failed
             }catch (Exception e){
                 e.printStackTrace();
             }
+            //operation will return the integer value for execute by onPostExecute method
             return bitSuccessConnect;
         }
     }
